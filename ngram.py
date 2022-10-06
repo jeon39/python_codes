bbb053a8762add71381293bdf0633a6e1a608e06
tmk_van <seongj.dship@gmail.com>
2022-10-06 16:34:36 +0900

    update ngram.py

--- ngram.py
+++ ngram.py
@@ -1,15 +1,18 @@
+class VanUtils:
+    def __init__(self):
+        return
 
-"""
-input : 입력된 문자열
-count : 잘라 생성할 문자열의 길이
-"""
-def ngram(input, count):
     """
-
-    for i in range(len(input) - count + 1):
-        r.append(input[i:i+count])
+    input : 입력된 문자열
+    count : 잘라 생성할 문자열의 길이
     """
+    def ngram(input, count):
+        """
 
-    rslt = [input[i:i+count] for i in range(len(input) - count + 1)]
+        for i in range(len(input) - count + 1):
+            r.append(input[i:i+count])
+        """
 
-    return rslt
+        rslt = [input[i:i+count] for i in range(len(input) - count + 1)]
+
+        return rslt
